e8b1f475508b9452271a35400796aec25f359213
noahtz <t0527184022@gmail.com>
2023-11-28 01:07:30 +0200

    add folder for media

--- setting.py
+++ setting.py
@@ -18,20 +18,20 @@ REFRESH_RATE = 60
 
 
 
-SOUND_GAME = 'music.wav'
-SOUND_SHOT = 'sound_shot_1.wav' # sound_shot_1.wav
+SOUND_GAME = 'media/music.wav'
+SOUND_SHOT = 'media/sound_shot_1.wav' # sound_shot_1.wav
 
 SCORE_INVADER = 100
 SCORE_SHOT = 1000
 SCORE_JOKER = 400
 SCORE_SHOT_SHIP = 1
 
-IMG_SHIP = pg.image.load('ship.bmp')
-IMG_INVADER_GREEN = pg.image.load('elian_green.png')
-IMG_INVADER_RED = pg.image.load('elian_red.png')
-IMG_SHOT = pg.image.load('shot.jpeg')
-IMG_JOKER = pg.image.load('joker.PNG')
-IMG_LIFE = pg.image.load('ship.bmp')
+IMG_SHIP = pg.image.load('media/ship.bmp')
+IMG_INVADER_GREEN = pg.image.load('media/elian_green.png')
+IMG_INVADER_RED = pg.image.load('media/elian_red.png')
+IMG_SHOT = pg.image.load('media/shot.jpeg')
+IMG_JOKER = pg.image.load('media/joker.PNG')
+IMG_LIFE = pg.image.load('media/ship.bmp')
 
 SPEED_MOVE_SHIP = 7
 SPEED_MOVE_INVADERS = 5
@@ -48,18 +48,18 @@ mef = True
 if mef:
     WIDTH, HEIGHT = 1600, 1158
     #WIDTH_PROTECTOR =
-    IMG_GAME = pg.image.load('img_game_mef.jpg')
-    IMG_GAME_OVER = pg.image.load('img_over_mef.jpg')
-    IMG_YOU_WINNER = pg.image.load('img_win_mef.png')
+    IMG_GAME = pg.image.load('media/img_game_mef.jpg')
+    IMG_GAME_OVER = pg.image.load('media/img_over_mef.jpg')
+    IMG_YOU_WINNER = pg.image.load('media/img_win_mef.png')
     ROWS_INVADERS = 4
     COLS_INVADERS = 10
     LOCATION_TEXT_GAME_Y = 0.6
     LOCATION_TEXT_GAME_X = 0.2
 else:
     WIDTH, HEIGHT = 960, 660
-    IMG_GAME = pg.image.load('img_game_home.jpeg')
-    IMG_GAME_OVER = pg.image.load('img_over_home.jpg')
-    IMG_YOU_WINNER = pg.image.load('img_win_home.png')
+    IMG_GAME = pg.image.load('media/img_game_home.jpeg')
+    IMG_GAME_OVER = pg.image.load('media/img_over_home.jpg')
+    IMG_YOU_WINNER = pg.image.load('media/img_win_home.png')
     ROWS_INVADERS = 3
     COLS_INVADERS = 6
     LOCATION_TEXT_GAME_Y = 0.65
